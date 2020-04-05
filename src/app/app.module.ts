800c337e30535d654006b406fd1cd91129f45905
Dmitrii <Dlob2@yandex.ru>
2020-04-05 18:53:26 +0300

    create first element

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ReportComponent } from './report/report.component';
+import { InventoryDescriptionComponent } from './inventory-description/inventory-description.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReportComponent,
+    InventoryDescriptionComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/inventory-description/inventory-description.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory-description',
+  templateUrl: './inventory-description.component.html',
+  styleUrls: ['./inventory-description.component.scss']
+})
+export class InventoryDescriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

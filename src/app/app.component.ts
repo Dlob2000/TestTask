67b0ff67816de81914a7f06dad1f93737947e6b5
Dmitrii <Dlob2@yandex.ru>
2020-04-07 23:39:49 +0300

    fix bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,34 +2,34 @@ import {Component, OnInit} from '@angular/core';
 import {FormArray, FormControl, FormGroup, FormsModule} from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
 
-  form: FormGroup;
+    form: FormGroup;
 
-  timeOptions = [
-    {
-      name: '1 month',
-      value: 31
-    }, {
-      name: '2 month',
-      value: 62
-    },
-  ];
+    timeOptions = [
+        {
+            name: '1 month',
+            value: 31
+        }, {
+            name: '2 month',
+            value: 62
+        },
+    ];
 
-  typeOfReport = ['Calibraton', 'Measuring', 'Using'];
+    typeOfReport = ['Calibraton', 'Measuring', 'Using'];
 
-  title = 'A-001234';
-  image = '../assets/scale.png';
-  description = 'Аналитические весы OHAUS Adventurer АХ324 (B715976163)'
+    title = 'A-001234';
+    image = '../assets/scale.png';
+    description = 'Аналитические весы OHAUS Adventurer АХ324 (B715976163)';
 
-  ngOnInit(): void {
-    this.form = new FormGroup({
-      period: new FormControl(31),
-      type: new FormControl(1)
-    });
-  }
+    ngOnInit(): void {
+        this.form = new FormGroup({
+            period: new FormControl(31),
+            type: new FormControl(1)
+        });
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { ReportComponent } from './report/report.component';
-import { InventoryDescriptionComponent } from './inventory-description/inventory-description.component';
+import {AppComponent} from './app.component';
+import {ReportComponent} from './report/report.component';
+import {InventoryDescriptionComponent} from './inventory-description/inventory-description.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ReportComponent,
-    InventoryDescriptionComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    ReactiveFormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ReportComponent,
+        InventoryDescriptionComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
